a5b0cc300b4123f4254f26dbda1fd6d36b77e847
Manuell00 <manuell.caselli@gmail.com>
2023-06-14 15:56:07 +0200

    Conclusione MainUsers

--- src/components/MainUsers.vue
+++ src/components/MainUsers.vue
@@ -4,6 +4,7 @@ import { store } from '../store.js';
 
 // Importo i COMPONENTI
 import SingleCardUser from './SingleCardUser.vue'
+import SingleCardAnimals from './SingleCardAnimals.vue'
 
 // Inserisco l'EXPORT
 export default {
@@ -11,6 +12,7 @@ export default {
 
     components: {
         SingleCardUser,
+        SingleCardAnimals,
     },
 
     data() {
@@ -36,6 +38,34 @@ export default {
                     name: "Sam Lewis"
                 },
 
+            ],
+
+            charactersListAnimals: [
+                {
+                    id: "1",
+                    src: "../../public/images/blog-1.jpg",
+                    description: "How to 'loose leash walk' your dog",
+                    date: "April 21st,2020"
+                },
+                {
+                    id: "2",
+                    src: "../../public/images/blog-2.jpg",
+                    description: "Stop cats from scratching furniture",
+                    date: "April 19th,2020"
+                },
+                {
+                    id: "3",
+                    src: "../../public/images/blog-3.jpg",
+                    description: "Where to buy the best pet food",
+                    date: "April 18th,2020"
+                },
+                {
+                    id: "4",
+                    src: "../../public/images/blog-4.jpg",
+                    description: "Proper care for your pet toys",
+                    date: "April 17th,2020"
+                },
+
             ]
         }
     },
@@ -45,7 +75,8 @@ export default {
 
 <!-- TEMPLATE -->
 <template>
-    <div class="container-fluid">
+    <!-- Definisco la prima sezione -->
+    <div class="container-fluid" id="first-container">
         <div class="container">
             <h2>Users testimonials</h2>
 
@@ -53,8 +84,40 @@ export default {
             <div class="user-testimonials">
                 <SingleCardUser v-for="character in charactersListUsers" :key="character.id" :detailsUser="character" />
             </div>
-
         </div>
+    </div>
+
+    <!-- Definisco la seconda sezione -->
+    <div class="container-fluid" id="second-container">
+
+        <!-- Inserisco il container di mezzo -->
+        <div class="container" id="middle-container">
+            <div class="text-letter">
+                <h2>Join our newsletter</h2>
+                <p>Volutpat vel turpis nulla lorem sed semper. Aliquam sagittis sem libero viverra vehicula nullam ut nisl.
+                </p>
+
+                <form>
+                    <input type="email" placeholder="Insert your email ...*">
+                    <button>Subscribe</button>
+                </form>
+            </div>
+        </div>
+
+        <!-- Inserisco il container per il continuo -->
+        <div class="container text-center" id="last-container">
+            <h2>Get the best tips & tricks</h2>
+            <span class="light">Recent articles</span>
+
+            <!-- Inserisco le card degli animals -->
+            <div class="animals">
+                <SingleCardAnimals v-for="character in charactersListAnimals" :key="character.id"
+                    :detailsAnimals="character" />
+            </div>
+
+            <button>Read all the articles</button>
+        </div>
+
     </div>
 </template>
 
@@ -65,13 +128,14 @@ export default {
 @use '../styles/general.scss';
 @use '../styles/partials/_variables.scss' as *;
 
-.container-fluid {
+#first-container {
     height: auto;
     width: 100%;
     background-color: $bg-darkgreen;
     color: white;
     padding-top: 100px;
     padding-bottom: 250px;
+    z-index: 1;
 
     .container {
         text-align: center;
@@ -92,8 +156,118 @@ export default {
         }
 
     }
+}
+
+#second-container {
+    height: auto;
+    width: 100%;
+    background-color: white;
+    color: black;
+    padding-bottom: 100px;
+    position: relative;
+    z-index: 2;
+
+    #middle-container {
+        height: 450px;
+        position: relative;
+        top: -100px;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        background-image: url(../../public/images/banner-7-2x-scaled.jpg);
+        background-size: cover;
+        padding-left: 50px;
+
+        h2 {
+            font-size: 60px;
+            font-weight: 200;
+            padding-bottom: 15px;
+        }
+
+        p {
+            padding-bottom: 25px;
+        }
 
 
+        .text-letter {
+            line-height: 1.8;
+            width: 45%;
+            height: auto;
 
+            input {
+                background-color: $bg-grey;
+                border-radius: 50px;
+                padding: 10px;
+                padding-right: 40px;
+                border: 0;
+                width: 350px;
+
+                &:hover {
+                    border-color: #999;
+                    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+                    transition: border-color 1s, box-shadow 1s;
+                }
+            }
+
+            button {
+                padding: 10px 20px;
+                background-color: $bg-green;
+                color: white;
+                font-weight: 500;
+                border: 0;
+                border-radius: 50px;
+                transition: all 0.3s ease-in-out;
+
+                &:hover {
+                    background-color: darken($bg-green, 10%);
+                    scale: (1.2);
+                }
+            }
+
+        }
+
+    }
+
+    #last-container {
+        width: 100%;
+        height: auto;
+
+        h2 {
+            font-size: 45px;
+        }
+
+        .light {
+            font-weight: 500;
+            opacity: 0.5;
+            font-size: 20px;
+        }
+
+        .animals {
+            margin: 40px 0;
+            height: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row;
+            flex-wrap: wrap;
+            gap: 20px;
+        }
+
+        button {
+            font-size: small;
+            padding: 10px 20px;
+            background-color: $bg-green;
+            color: white;
+            font-weight: 500;
+            border: 0;
+            border-radius: 50px;
+            transition: all 0.3s ease-in-out;
+
+            &:hover {
+                background-color: darken($bg-green, 10%);
+                scale: (1.2);
+            }
+        }
+    }
 }
 </style>
---
+++ src/components/SingleCardAnimals.vue
@@ -0,0 +1,85 @@
+<script>
+// Importo lo STORE
+import { store } from '../store.js';
+
+
+// Inserisco l'EXPORT
+export default {
+    name: 'SingleCardAnimals',
+    props: {
+        detailsAnimals: Object
+    },
+
+    data() {
+        return {
+            store,
+        }
+    },
+
+}
+</script>
+
+<!-- TEMPLATE -->
+<template>
+    <div class="prod-card">
+        <div class="img-container">
+            <img :src="detailsAnimals.src" class="img">
+        </div>
+        <h4>{{ detailsAnimals.description }}</h4>
+        <span class="light">{{ detailsAnimals.date }}</span>
+    </div>
+</template>
+  
+<style scoped lang="scss">
+@use '../styles/general.scss';
+@use '../styles/partials/_variables.scss' as *;
+
+.prod-card {
+    width: calc((100% / 4) - 20px);
+    height: auto;
+    border: 2px solid lighten(grey, 70%);
+    transition: background-color 0.6s;
+    text-align: left;
+
+
+    h4 {
+        padding-top: 20px;
+        font-weight: 300;
+        color: black;
+    }
+
+    .light {
+        opacity: 0.7;
+        font-size: 12px;
+    }
+
+    .oldprice {
+        font-size: 13px;
+        text-decoration: line-through;
+        margin-right: 10px;
+        opacity: 0.7;
+    }
+
+    .img-container {
+        width: 100%;
+        height: 100%;
+        /* Regola l'altezza del container immagine come preferisci */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: cover;
+        }
+
+    }
+
+    &:hover {
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+        transform: translateY(-5px);
+    }
+}
+</style>
+  
